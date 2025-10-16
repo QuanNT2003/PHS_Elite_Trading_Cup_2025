b3d0c1f704491e34fb28912cab50bfe160d174f6
Quan Ngo <ngotrungquan1412@gmail.com>
2025-10-16 14:38:19 +0700

    PlaywrightSetup

---
+++ src/test/invalid_accountNumber.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test("Test_Invalid_AccountNumber", async ({ page }) => {
+  await page.goto("http://localhost:3001/");
+  await page.waitForTimeout(1000);
+  await page.getByRole("button", { name: "Đăng ký" }).click();
+  await page.waitForTimeout(1000);
+  await page.locator("#AccountNumber").click();
+  await page.locator("#AccountNumber").fill("0465");
+  await page.waitForTimeout(1000);
+  await page.locator("#Email").click();
+  await page.locator("#Email").fill("ngotrungquan@gmail.com");
+  await page.waitForTimeout(1000);
+  await page.locator("#Phone").click();
+  await page.locator("#Phone").fill("0365754657");
+  await page.waitForTimeout(1000);
+  await page.getByRole("button", { name: "Kiểm tra" }).click();
+  await page.waitForTimeout(1000);
+});
